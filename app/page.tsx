c128e3bba159edc37087400bbd313facdb315e5d
Moscart <danieltheo.73@gmail.com>
2024-08-30 16:55:42 +0700

    ✨feature: Create product page

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,198 @@
+"use client";
+
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import {
+  Carousel,
+  type CarouselApi,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { cn } from "@/lib/utils";
+import { Minus, Plus, ShoppingCart } from "lucide-react";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+  const [api, setApi] = useState<CarouselApi>();
+  const [dialogApi, setDialogApi] = useState<CarouselApi>();
+  const [current, setCurrent] = useState(0);
+  const [dialogCurrent, setDialogCurrent] = useState(0);
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    setDialogCurrent(api.selectedScrollSnap());
+    dialogApi?.scrollTo(api.selectedScrollSnap());
+
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap());
+    });
+
+    dialogApi?.on("select", () => {
+      setDialogCurrent(dialogApi.selectedScrollSnap());
+    });
+  }, [api, dialogApi]);
+
+  const Navigation = ({
+    className,
+    api,
+    current,
+  }: {
+    className?: string;
+    current?: number;
+    api?: CarouselApi;
+  }) => (
+    <div className={cn(`hidden sm:flex justify-between pt-6`, className)}>
+      {[1, 2, 3, 4].map((_, index) => (
+        <div
+          className={`rounded-lg bg-white ${
+            index === current ? "ring-4 ring-primary" : ""
+          }`}
+        >
+          <Image
+            src={`/assets/image-product-${index + 1}-thumbnail.jpg`}
+            alt="Product 1"
+            height={80}
+            width={80}
+            onDragStart={(e) => e.preventDefault()}
+            className={`sm:w-28 sm:h-3w-28 lg:w-20 lg:h-20 xl:w-24 xl:h-24  rounded-lg hover:opacity-50 ${
+              index === current ? "opacity-50" : ""
+            }`}
+            onClick={() => api?.scrollTo(index)}
+          />
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      ))}
+    </div>
+  );
+  return (
+    <main className="min-h-[calc(100vh_-_72px)] lg:min-h-[calc(100vh_-_122px)]">
+      <div className="sm:py-10 lg:py-28 sm:max-w-lg lg:max-w-6xl sm:mx-auto lg:px-10 lg:grid lg:grid-cols-2">
+        <Carousel
+          opts={{ watchDrag: false }}
+          className="lg:px-10"
+          setApi={setApi}
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <Dialog>
+            <DialogTrigger
+              onClick={(e) => window.innerWidth <= 640 && e.preventDefault()}
+            >
+              <CarouselContent>
+                {[1, 2, 3, 4].map((_, index) => (
+                  <CarouselItem key={_}>
+                    <Image
+                      alt="product"
+                      src={`/assets/image-product-${index + 1}.jpg`}
+                      width={1000}
+                      height={1000}
+                      onDragStart={(e) => e.preventDefault()}
+                      className="aspect-[4/3] lg:aspect-square object-cover object-top w-full sm:rounded-lg"
+                    />
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+            </DialogTrigger>
+            <DialogContent>
+              <Carousel
+                opts={{ loop: true, watchDrag: false }}
+                className=""
+                setApi={setDialogApi}
+              >
+                <CarouselContent>
+                  {[1, 2, 3, 4].map((_, index) => (
+                    <CarouselItem key={_}>
+                      <Image
+                        alt="product"
+                        src={`/assets/image-product-${index + 1}.jpg`}
+                        width={1000}
+                        height={1000}
+                        onDragStart={(e) => e.preventDefault()}
+                        className="aspect-square sm:rounded-lg"
+                      />
+                    </CarouselItem>
+                  ))}
+                </CarouselContent>
+                <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full">
+                  <CarouselPrevious className="-left-4 scale-150" />
+                  <CarouselNext className="-right-4 scale-150" />
+                </div>
+              </Carousel>
+              <Navigation
+                className="pt-2"
+                current={dialogCurrent}
+                api={dialogApi}
+              />
+            </DialogContent>
+          </Dialog>
+          <div className="sm:hidden absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full">
+            <CarouselPrevious className="left-6" />
+            <CarouselNext className="right-6" />
+          </div>
+          <Navigation current={current} api={api} />
+        </Carousel>
+        <div className="p-6 flex flex-col gap-6 sm:p-0 sm:pt-10 lg:px-10 lg:justify-center">
+          <div className="grid gap-2 lg:gap-6">
+            <div className="font-bold text-xs lg:text-sm text-gray-500 tracking-[0.15rem] uppercase">
+              Sneaker Company
+            </div>
+            <div className="font-bold text-3xl lg:text-5xl">
+              Fall Limited Edition Sneakers
+            </div>
+            <p className="text-gray-500 text-pretty text-[15px] lg:text-base">
+              These low-profile sneakers are your perfect casual wear companion.
+              Featuring a durable rubber outer sole, they'll withstand
+              everything the weather can offer.
+            </p>
+          </div>
+          <div className="font-bold flex lg:flex-col lg:items-start lg:justify-normal lg:gap-2 justify-between items-center">
+            <div className="flex gap-3">
+              <div className="text-3xl">$125.00</div>
+              <Badge className="h-fit bg-black hover:bg-black self-center">
+                50%
+              </Badge>
+            </div>
+            <div className="line-through text-gray-500">$250.00</div>
+          </div>
+          <div className="grid gap-4 sm:grid-cols-2">
+            <div className="grid grid-cols-5 sm:grid-cols-3 bg-gray-100 rounded-md overflow-hidden">
+              <Button
+                className="col-span-1 px-0 bg-transparent rounded-r-none group hover:bg-transparent"
+                size={"lg"}
+              >
+                <Minus
+                  className="h-4 w-4 text-primary group-hover:text-primary/50 transition-all"
+                  strokeWidth={3}
+                />
+              </Button>
+              <input
+                type="number"
+                className="col-span-3 sm:col-span-1 bg-transparent text-center font-bold flex-grow focus:outline-none"
+                min={0}
+                defaultValue={0}
+              />
+              <Button
+                className="col-span-1 px-0 bg-transparent rounded-l-none group hover:bg-transparent"
+                size={"lg"}
+              >
+                <Plus
+                  className="h-4 w-4 text-primary group-hover:text-primary/50 transition-all"
+                  strokeWidth={3}
+                />
+              </Button>
+            </div>
+            <Button
+              className="w-full font-bold text-black shadow-primary/50 shadow-lg"
+              size={"lg"}
+            >
+              <ShoppingCart className="w-4 h-4 me-3" strokeWidth={3} />
+              Add to cart
+            </Button>
+          </div>
+        </div>
       </div>
     </main>
   );
